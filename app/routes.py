32967dd2a946ec6f8361521881c680c67e906bb7
TheGreatSkeeve <72765359+TheGreatSkeeve@users.noreply.github.com>
2021-02-05 00:07:39 -0500

    1.1

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,10 @@ from BOFH import *
 import random
 
 
-
 @app.route('/')
 def index():
     x = random.randint(0,465)
-    response=bofh_text[x]
-    return render_template('index.html', title='IT Help Desk', response=response)
+    response=bofh_text[x].capitalize()
+    lmgtfy="https://www.google.com/search?q=" + response.replace(" ","+")
+
+    return render_template('index.html', title='IT Help Desk', response=response, link=lmgtfy)
